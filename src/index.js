96d8cbf1bfe70e96778759b82744fdfb778da124
Avery-techdev <Avery-techdev@users.noreply.github.com>
2021-02-03 10:28:12 +0100

    added weather consition description

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function displayWeatherCondotion(response) {
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
 }
 
 function searchCity(city) {
